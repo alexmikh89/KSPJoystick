8b1623119db1a9d25eac5d287ad8c038380ea5cf
Oleksii Mykahailenko <alexmikh89@gmail.com>
2024-01-17 19:45:49 +0200

    environment customization

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,23 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("Hi, we are in The Setup");
+  delay(2000);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  Serial.println("Hi, we are in the Loop");
+
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+
+  delay(500);
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
